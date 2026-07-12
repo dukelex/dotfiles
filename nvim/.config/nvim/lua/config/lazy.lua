3c21bd9bf5de7668ba358edc5bebe172f5914b77
Alexey Kukarsky <alexey@kukarsky.ru>
2026-07-12 09:34:12 +0500

    add surround plugin

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -101,6 +101,17 @@ require("lazy").setup({
         dependencies = 'nvim-tree/nvim-web-devicons'}
     },
     {
+        "kylechui/nvim-surround",
+        version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        -- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+        -- config = function()
+        --     require("nvim-surround").setup({
+        --         -- Put your configuration here
+        --     })
+        -- end
+    },
+    {
         {
           'nvimdev/dashboard-nvim',
           event = 'VimEnter',
